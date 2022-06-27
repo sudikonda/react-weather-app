ebc9c8fc8d43b5657d5559af5a059fe3cfb9d8a9
sudikonda <sudikonda.sivakumar@gmail.com>
2022-06-26 20:17:00 -0400

    feat[search]: clear search input after enter

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -2,38 +2,38 @@ import React, { useState } from "react";
 import { UilSearch, UilLocationPoint } from "@iconscout/react-unicons";
 import { toast } from "react-toastify";
 
-function Inputs({setQuery, units, setUnits}) {
-
-  const [city, setCity] = useState('');
+function Inputs({ setQuery, units, setUnits }) {
+  const [city, setCity] = useState("");
 
   const handleSearchClick = () => {
-    if(city !== '')
-      setQuery({q: city})
+    if (city !== "") {
+      setQuery({ q: city });
+      setCity('');
+    }
   };
 
   const handleSearchClickEnter = (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       handleSearchClick();
     }
   };
 
   const handleLocationClick = () => {
-    if(navigator.geolocation){
-      toast.info('Fetching users location');
-      navigator.geolocation.getCurrentPosition(position => {
-        toast.success('Location was successfully fetched');
+    if (navigator.geolocation) {
+      toast.info("Fetching users location");
+      navigator.geolocation.getCurrentPosition((position) => {
+        toast.success("Location was successfully fetched");
         let lat = position.coords.latitude;
         let lon = position.coords.longitude;
 
-        setQuery({lat, lon});
-      })
+        setQuery({ lat, lon });
+      });
     }
   };
 
   const handleMetricClick = (e) => {
     const selectedUnit = e.currentTarget.name;
-    if(units !== selectedUnit)
-      setUnits(selectedUnit);
+    if (units !== selectedUnit) setUnits(selectedUnit);
   };
 
   return (
@@ -44,17 +44,37 @@ function Inputs({setQuery, units, setUnits}) {
           className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
           placeholder="Search for city..."
           value={city}
-          onChange={e => setCity(e.currentTarget.value)}
+          onChange={(e) => setCity(e.currentTarget.value)}
           onKeyDown={handleSearchClickEnter}
         />
-        <UilSearch size={25} className='text-white cursor-pointer transition ease-out hover:scale-125' onClick={handleSearchClick}/>
-        <UilLocationPoint size={25} className='text-white cursor-pointer transition ease-out hover:scale-125' onClick={handleLocationClick}/>
+        <UilSearch
+          size={25}
+          className="text-white cursor-pointer transition ease-out hover:scale-125"
+          onClick={handleSearchClick}
+        />
+        <UilLocationPoint
+          size={25}
+          className="text-white cursor-pointer transition ease-out hover:scale-125"
+          onClick={handleLocationClick}
+        />
       </div>
 
       <div className="flex flex-row w-1/4 items-center justify-center">
-        <button name="metric" className="text-xl text-white font-light transition ease-out hover:scale-125" onClick={handleMetricClick}>℃</button>
+        <button
+          name="metric"
+          className="text-xl text-white font-light transition ease-out hover:scale-125"
+          onClick={handleMetricClick}
+        >
+          ℃
+        </button>
         <p className="text-xl text-white mx-1">|</p>
-        <button name="imperial" className="text-xl text-white font-light transition ease-out hover:scale-125" onClick={handleMetricClick}>℉</button>
+        <button
+          name="imperial"
+          className="text-xl text-white font-light transition ease-out hover:scale-125"
+          onClick={handleMetricClick}
+        >
+          ℉
+        </button>
       </div>
     </div>
   );
